84ccae164a7a429c70444f50025584d5a6d99cce
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2025-07-12 16:58:06 -0300

    create initial app structure

---
+++ src/test/java/br/edu/ufrn/cities/CitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufrn.cities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
